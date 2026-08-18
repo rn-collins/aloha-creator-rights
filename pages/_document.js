fa944cf48b12ce2b5a304a74c91fe52873545f9c
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-18 08:54:04 -1000

    Add branded favicon metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="theme-color" content="#1B7A68" />
         <script defer src="/_vercel/insights/script.js" />
         <script defer src="/_vercel/speed-insights/script.js" />
       </Head>
